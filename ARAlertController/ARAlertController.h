c3bbf09634286f58c4007809fd8c40240b3e4cd2
alexruperez <alexruperez@gmail.com>
2015-02-14 13:13:49 +0100

    Initial Commit

---
+++ ARAlertController/ARAlertController.h
@@ -0,0 +1,19 @@
+//
+//  ARAlertController.h
+//  ARAlertController
+//
+//  Created by alexruperez on 14/2/15.
+//  Copyright (c) 2015 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARAlertController.
+FOUNDATION_EXPORT double ARAlertControllerVersionNumber;
+
+//! Project version string for ARAlertController.
+FOUNDATION_EXPORT const unsigned char ARAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARAlertController/PublicHeader.h>
+
+
